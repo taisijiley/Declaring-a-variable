443855df5141889bd4797a4b9727dcfa9b395ba7
Teren <Official.Teren@yandex.ru>
2024-04-27 19:36:54 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a = 13; //объ€вл€ем переменную "a"
+	cout << a << endl; // выводим ее
+
+	// другой способ
+
+	int b; 
+	b = 13;
+	cout << b << endl;
+
+	// еще один
+
+	int i = 0; 
+	i++;
+	cout << i << endl;
+}
